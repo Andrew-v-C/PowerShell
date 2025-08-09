0f8078db6ca3cc756a75163c18ffce7c8f09f888
Andrew-v-C <andrewvancampen@gmail.com>
2025-08-09 12:01:42 -0400

    Added comments

--- Profile.ps1
+++ Profile.ps1
@@ -1,3 +1,5 @@
+
+# Allow "Duplicate tab" to work
 function loc { return $executionContext.SessionState.Path.CurrentLocation; }
 function defPrompt { return "PS $(loc)$('>' * ($nestedPromptLevel + 1)) "; }
 function prompt {
@@ -8,15 +10,22 @@ function prompt {
     $out += $(defPrompt);
     return $out;
 }
+
+# Create custom command to enter VS Developer PowerShell
 function openDevShell {
     pwsh -noe -c {
         Import-Module "C:/Program Files (x86)/Microsoft Visual Studio/2022/BuildTools/Common7/Tools/Microsoft.VisualStudio.DevShell.dll";
-        Enter-VsDevShell 52711d80;
+        Enter-VsDevShell 52711d80;  # Will need to modify this line; refer to the "Developer PowerShell for VS" shortcut
         function prompt {
             return "$($PSStyle.Foreground.Magenta)DEV$($PSStyle.Reset) $(defPrompt)";
         }
     }
 }
 Set-Alias -Name dev -Value openDevShell;
-Set-Content -Path (Get-PSReadLineOption).HistorySavePath -Value $null
+
+# Modify style
 $PSStyle.FileInfo.Directory = "`e[34;1m"
+
+# Clear command history
+Set-Content -Path (Get-PSReadLineOption).HistorySavePath -Value $null
+
