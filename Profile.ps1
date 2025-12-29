b273fb837a7795c72d4f4ebbba01e8c9a1df751b
Andrew-v-C <andrewvancampen@gmail.com>
2025-12-29 13:44:27 -0500

    Removed command to enter VS shell

--- Profile.ps1
+++ Profile.ps1
@@ -1,17 +1,4 @@
 
-# Create custom command to enter VS Developer PowerShell
-function defPrompt { return "PS $(loc)$('>' * ($nestedPromptLevel + 1)) "; }
-function openDevShell {
-    pwsh -noe -c {
-        Import-Module "C:/Program Files (x86)/Microsoft Visual Studio/2022/BuildTools/Common7/Tools/Microsoft.VisualStudio.DevShell.dll";
-        Enter-VsDevShell b0e67882;  # Will need to modify this line; refer to the "Developer PowerShell for VS" shortcut
-        function prompt {
-            return "$($PSStyle.Foreground.Magenta)DEV$($PSStyle.Reset) $(defPrompt)";
-        }
-    }
-}
-Set-Alias -Name dev -Value openDevShell;
-
 # Modify style
 $PSStyle.FileInfo.Directory = "`e[34;1m"
 
