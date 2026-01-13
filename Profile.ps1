28078803ea0b5ddc24925038a910efa1bc8bc7cf
Andrew-v-C <andrewvancampen@gmail.com>
2026-01-13 14:46:13 -0500

    Removed extra blank lines

--- Profile.ps1
+++ Profile.ps1
@@ -1,8 +1,6 @@
-
 # Modify style
 $PSStyle.FileInfo.Directory = "`e[34;1m"
 
 # Clear command history, and don't use it as a completion source
 Set-Content -Path (Get-PSReadLineOption).HistorySavePath -Value $null
 Set-PSReadLineOption -PredictionSource None
-
