b6a2e950c4c0a8fd3dff915b6e64ccb1503dc4f7
Andrew-v-C <andrewvancampen@gmail.com>
2026-02-03 23:18:23 -0500

    Changes

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,16 @@
-# Modify style
-$PSStyle.FileInfo.Directory = "`e[34;1m"
+# Create function to open Neovim and reset cursor on exit
+function ed {
+    & "$HOME\Software\Neovim\bin\nvim" $args
+    Write-Host -NoNewLine "`e[5 q"
+}
 
 # Create variables for convenience
 $docs = "$HOME\OneDrive\Documents"
 $proj = "$HOME\Projects"
 
+# Modify style
+$PSStyle.FileInfo.Directory = "`e[34;1m"
+
 # Clear command history, and don't use it as a completion source
 Set-Content -Path (Get-PSReadLineOption).HistorySavePath -Value $null
 Set-PSReadLineOption -PredictionSource None
-
-# Create function to open Neovim, and reset cursor on exit
-function nvim {
-    & "$HOME\Software\nvim-win64\bin\nvim" $args
-    Write-Host -NoNewLine "`e[5 q"
-}
